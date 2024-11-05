e561ec6d87e3970e0b266cec971d1bb7e98998cb
Vinicius Marchand <105612662+ViniciusMarchand@users.noreply.github.com>
2024-11-05 00:39:44 +0000

    delete: delete project

--- TRAB_PPA/README.md
+++
@@ -1,1 +0,0 @@
-# aula-03-ordem-das-dependencias-e-padroes-de-persistencia
--- TRAB_PPA/src/main/java/br/edu/ifrs/riogrande/tads/ppa/ligaa/repository/EntidadeInativaException.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifrs.riogrande.tads.ppa.ligaa.repository;
-
-public class EntidadeInativaException extends RuntimeException {
-
-    public EntidadeInativaException() {
-        super("A entidade não pode ser operacionalizada porque está inativa");
-    }
-
-}
